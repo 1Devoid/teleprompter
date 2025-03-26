72e9a9a6e7d8bfd1474f6a610feda09f6cf7733c
Andrii Zdrobylko <the.az.dev@gmail.com>
2025-03-26 22:16:55 +0200

    [UPDATE] Creating record_screen.dart. Record and stoping record states initialized(only dev issue)

--- lib/flows/record/presentation/logic/record/record_state.dart
+++ lib/flows/record/presentation/logic/record/record_state.dart
@@ -1,12 +1,14 @@
 part of 'record_cubit.dart';
 
-enum RecordStatus {init, loading, success, failure}
+enum RecordStatus { init, loading, success, failure }
 
 class RecordState extends Equatable {
   const RecordState({
     this.status = RecordStatus.init,
     this.cameras = const [],
     this.selectedCamera = 0,
+    this.isRecording = false,
+    this.recordingTime = '00:00:00',
     this.scrollTimeMill = 0,
     this.currentScroll = 0.0,
     this.errorMessage,
@@ -15,7 +17,9 @@ class RecordState extends Equatable {
   final RecordStatus status;
   final List<CameraDescription> cameras;
   final int selectedCamera;
-  final int scrollTimeMill; // duration in milliseconds
+  final bool isRecording;
+  final String recordingTime;
+  final int scrollTimeMill; // scroll duration in milliseconds
   final double currentScroll;
   final String? errorMessage;
 
@@ -27,15 +31,19 @@ class RecordState extends Equatable {
     int? scrollTimeMill,
     double? currentScroll,
     int? selectedCamera,
-    String? errorMessage
-  }){
+    bool? isRecording,
+    String? recordingTime,
+    String? errorMessage,
+  }) {
     return RecordState(
       status: status ?? this.status,
       cameras: cameras ?? this.cameras,
       currentScroll: currentScroll ?? this.currentScroll,
       scrollTimeMill: scrollTimeMill ?? this.scrollTimeMill,
       selectedCamera: selectedCamera ?? this.selectedCamera,
-      errorMessage: errorMessage ?? this.errorMessage
+      isRecording: isRecording ?? this.isRecording,
+      recordingTime: recordingTime ?? this.recordingTime,
+      errorMessage: errorMessage ?? this.errorMessage,
     );
   }
 
